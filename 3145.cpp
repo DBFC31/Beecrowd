f999646935f54de0c6fca0feb8cd9e4ac94a9e2d
DBFC31 <danielbaracatt@gmail.com>
2023-05-08 18:29:26 -0300

    a

--- 3145.cpp
+++ 3145.cpp
@@ -1,1 +1,12 @@
 #include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    a += 2;
+    cout << fixed << setprecision(2) << (float)b / a << endl;
+    return 0;
+}
