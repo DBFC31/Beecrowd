662dc2349ec7e769de095b1b0ea3c626029b11fe
DBFC31 <danielbaracatt@gmail.com>
2023-05-09 17:25:56 -0300

    a

--- i3148.cpp
+++ i3148.cpp
@@ -2,51 +2,43 @@
 
 using namespace std;
 
-vector<int> X;
-
-int kills(int exp, int i)
-{
-    int s = 0;
-    for (int j = 0; j < X.size(); j++)
-    {
-        if (j == i)
-            continue;
-        if (X[j] == -1)
-            continue;
-        s += X[j];
-    }
-    return exp * s;
-}
-
 int main()
 {
     int t;
     cin >> t;
-    while (t--)
+    while (t > 0)
     {
         int n;
         cin >> n;
+        int X[n];
+        int to = 0; // total de virus
+        int k = 0;  // total de virus que seriam matados
         for (int i = 0; i < n; i++)
         {
             int a;
             cin >> a;
-            X.push_back(a);
+            X[i] = a;
+            to += a;
         }
+        k = to;
 
         int exp = 1; // número de vírus mortos por dedetização = X[i] * exp
-        sort(X.begin(), X.end());
+        sort(X, X + n);
         for (int i = 0; i < n; i++)
         {
-            if (kills(exp + 1, i) > kills(exp, -1)) // se adquirir a experiencia valer mais a pena que detetizar
+            if (X[i] == -1)
+                continue;
+            if ((to - X[i]) * (exp + 1) > k) // se adquirir a experiencia valer mais a pena que detetizar
             {
                 exp++;
+                to -= X[i];
+                k = to * exp;
                 X[i] = -1;
             }
         }
-        cout << kills(exp, -1) << endl;
-        X.clear();
+        cout << k << endl;
+        t -= 1;
     }
 
     return 0;
-    // ideia: considerar a soma inicial e inves de recalcular, subtrair Xi e multiplicar por 2 (no primeiro caso)
 }
